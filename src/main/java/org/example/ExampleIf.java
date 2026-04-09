c5f2921fa8d61b593f81f379ea285a3175bb7975
Tsiu11 <Tsiu11@wgu.edu>
2026-04-09 14:52:53 -0400

    if statement

---
+++ src/main/java/org/example/ExampleIf.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class ExampleIf {
+    public static void main(String[] args) {
+        //if statement
+        //Pseudo code first
+        //if person is over 50 old
+        //if the person is between 19-49
+
+    }
+}
